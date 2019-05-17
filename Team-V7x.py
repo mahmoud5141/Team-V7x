1e6031ffcfd4f0bdd759cb28d064542de843eccb
Mahmoud <50506318+Mahmoud514@users.noreply.github.com>
2019-05-17 15:56:22 +0200

    Update Team-V7x.py

--- Team-V7x.py
+++ Team-V7x.py
@@ -1,6 +1,6 @@
 
 #!usr/bin/etc python# -*- coding: utf-8 -*-
-import osimport timeyehia = 1XXX = 1
+import osimport timeyehia = 1234 = 1
 #[ Warning, The Failed Thief The Script is Not Encrypted
 Team-V7x= (1017710847770001110510767482578937857289462829372380014678809345809345803580954304898306548093473947293407402309246653777)
 
